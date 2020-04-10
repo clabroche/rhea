4402cd0c7b9cf972b0e3f506cc1b240903ec3937
clabroche <corentin.labroche@sailendra.fr>
2020-04-10 02:34:42 +0200

    fix(deploy): update actions

--- android/build.sh
+++ android/build.sh
@@ -2,6 +2,5 @@
 cd ..
 npx cap sync
 cd android
-rm app/build/outputs/apk/release/*.apk
 ./gradlew assembleRelease
-apksigner sign --ks-pass file:keystorepass --ks ~/keystore.jks  --out  ../server/public/rhea.apk app/build/outputs/apk/release/app-release-unsigned.apk
+  apksigner sign --ks-pass file:keystorepass --ks ~/keystore.jks  --out  ../server/public/rhea.apk app/build/outputs/apk/release/app-release-unsigned.apk
