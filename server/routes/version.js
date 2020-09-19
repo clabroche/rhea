21a00ce8afddf0db6b047045af839ce45050a6e9
clabroche <corentinlabroche@gmail.com>
2020-09-19 17:15:38 +0200

    fix(version): trim version before send

--- server/routes/version.js
+++ server/routes/version.js
@@ -6,7 +6,7 @@ const pathfs = require('path')
 router.get('/', async function (req, res) {
   const version = await fse.readFile(pathfs.resolve(__dirname, '..', 'version'))
   console.log(version)
-  res.send(version)
+  res.send(version.trim())
 })
 
 module.exports = router
